eeae65d7df842fc4993f3d2fa5a6c118730b4c88
Elizabeth Fredenburg <efredenburg4065@mail.kvcc.edu>
2011-04-27 21:32:41 -0400

    error free

---
+++ controllers/IndexController.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ *
+ *
+ * @author Brendan Howard
+ * @category ANM293 - Advanced PHP
+ *
+ * Final Exam
+ *
+ */
+
+  class IndexController extends Zend_Controller_Action
+  {
+
+      public function init()
+      {
+
+      }
+
+      public function defaultAction()
+      {
+        header("Content-Type: text/html;charset=utf-8");
+        require_once APP_PATH . SLASH . 'views' . SLASH . 'index.phtml';
+      }
+
+  }
